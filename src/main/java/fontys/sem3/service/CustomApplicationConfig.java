816ea0b231287540f2d99ee7e97787cfb8071bb0
VladimirCVB <47753522+VladimirCVB@users.noreply.github.com>
2020-11-20 12:39:09 +0200

    cors filter fix + comment fix

--- src/main/java/fontys/sem3/service/CustomApplicationConfig.java
+++ src/main/java/fontys/sem3/service/CustomApplicationConfig.java
@@ -15,6 +15,9 @@ public class CustomApplicationConfig extends ResourceConfig
 	{
 		packages("fontys.sem3.service.resources"); // find all resource endpoint classes in this package
 		// log exchanged http messages
+
+/*		If there is a CORS error you have to comment the AuthenticationFilter registration here
+		and run the server, afterwards, stop the server, uncomment the filter and run again */
 		register(AuthenticationFilter.class);
 		register(CorsFilter.class);
 		register(new LoggingFeature(Logger.getLogger(LoggingFeature.DEFAULT_LOGGER_NAME),
